8e7589258c68da0056d027e15f98408f437edab1
Jacob Priddy <jacob.priddy@wallawalla.edu>
2018-02-28 18:57:49 -0800

    Added register definitions

--- Assembler/Assembler/assembler.h
+++ Assembler/Assembler/assembler.h
@@ -1,6 +1,15 @@
 #ifndef ASSEMBLER_H_   /* Include guard */
 #define ASSEMBLER_H_
 
-
+typedef enum {
+PC = 0x0;
+R1 = 0x1;
+R2 = 0x2;
+R3 = 0x3;
+R4 = 0x4;
+R5 = 0x5;
+R6 = 0x6;
+R7 = 0x7;
+} REGISTERS;
 
 #endif // ASSEMBLER_H_

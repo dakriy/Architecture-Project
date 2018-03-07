66e3c3b8db4124c0a7a3905a8723734c92f0f2c6
chiq2045 <Charles.Oroko@outlook.com>
2018-03-06 19:14:42 -0800

    added core file

--- Architecture/src/cpu_core.vhd
+++ Architecture/src/cpu_core.vhd
@@ -34,29 +34,29 @@ entity cpu_core is
     port(
           i_clk     : in  std_logic; --global clock
           i_clr     : in  std_logic;
-          i_intvec  : in  std_logic_vector(  5 downto 0 ); --interupt vector input
+          --i_intvec  : in  std_logic_vector(  5 downto 0 ); --interupt vector input
           i_din     : in  std_logic_vector(  7 downto 0 ); --input data (pmem or i/o)
 
-          q_opc     : out std_logic_vector( 15 downto 0 );
-          q_pc      : out std_logic_vector( 15 downto 0 );
-          q_dout    : out std_logic_vector(  7 downto 0 );
-          q_adr_io  : out std_logic_vector(  7 downto 0 );
-          q_rd_io   : out std_logic;
-          q_we_io   : out std_logic
+          q_opc     : out std_logic_vector( 15 downto 0 ); --current opcode
+          q_pc      : out std_logic_vector( 15 downto 0 ); --current program counter
+          q_dout    : out std_logic_vector(  7 downto 0 ); --output data
+          --q_adr_io  : out std_logic_vector(  5 downto 0 ); --address of i/o register (32 8-bit registers)
+          q_rd_io   : out std_logic; --select register to read
+          q_we_io   : out std_logic --select register to write
     );
 end cpu_core;
 
 architecture Behavioral of cpu_core is
 component opc_fetch
-    port(
-          i_clk     : in std_logic;
+  port(
+        i_clk     : in std_logic;
 
-          i_clr     : in std_logic_vector(  5 downto 0 );
-          i_intvec  : in std_logic_vector( 15 downto 0 );
-          i_new_pc  : in std_logic_vector( 15 downto 0 );
-          i_load_pc : in std_logic;
-          i_pm_addr : in
-          );
+        i_clr     : in std_logic_vector(  5 downto 0 );
+        i_intvec  : in std_logic_vector( 15 downto 0 );
+        i_new_pc  : in std_logic_vector( 15 downto 0 );
+        i_load_pc : in std_logic;
+        i_pm_addr : in 
+  );
 
 
 l_din     <= f_pm_dout when (d_pms = '1') else i_din(7 downto 0); --mux selects between pmem and i/o

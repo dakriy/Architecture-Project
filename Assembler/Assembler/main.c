1ec88974535e7c564671daff976582dd969c5367
dakriy <git@persignum.com>
2018-03-13 01:33:12 -0700

    I am mother flippin Sun Tzu baby!

--- Assembler/Assembler/main.c
+++ Assembler/Assembler/main.c
@@ -54,13 +54,12 @@ void outputToFile(char * fileName, instruction * data, unsigned short instructio
 
 		for (int i = 0; i < instructionCount; i++)
 		{
-			// TODO: GET AN ASRPRIN AND FIX THIS. HOOLY MOOOLLLYYY
+			// TODO: Make sure this is right
 			unsigned char buff[2];
 			// Make sure I don't get screwed over by big/little endianness
 			// LATER EDIT: I'm getting screwed by it anyway.... This is probably only going to work on little endian machines...
-			// GAAAAAAAAAAH I CAN'T WIN SERIOUSLY
-			buff[0] = (data[i].O & 0xF000) >> 12 | (data[i].O & 0x0F00) >> 4;
-			buff[1] = (data[i].O & 0x00F0) >> 4 | (data[i].O & 0x000F) << 4;
+			buff[1] = (data[i].O & 0xF000) >> 12 | (data[i].O & 0x0F00) >> 4;
+			buff[0] = (data[i].O & 0x00F0) >> 4 | (data[i].O & 0x000F) << 4;
 			if(fwrite(buff, sizeof(char), sizeof buff, f) != sizeof(instruction))
 			{
 				perror("Error writing to file");
